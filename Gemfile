6b7364be082d6e1fe058b4350fa48b37973444df
tukaram40k <tukaram40k@gmail.com>
2025-11-23 20:30:30 +0200

    add nyaplot dependency

--- Gemfile
+++ Gemfile
@@ -5,3 +5,7 @@ source "https://rubygems.org"
 gem "sinatra", "~> 4.2"
 gem "rackup", "~> 2.2"
 gem "puma", "~> 7.1"
+
+gem "nyaplot", "~> 0.1.6"
+
+gem "csv", "~> 3.3"
--- main.rb
+++ main.rb
@@ -1,1 +1,12 @@
-puts 3 + 2
+require 'nyaplot'
+
+# sample data
+x = [1, 2, 3, 4, 5, 6, 7]
+y = [3, 7, 4, 9, 6, 56, 44]
+
+plot = Nyaplot::Plot.new
+plot.add(:line, x, y)
+
+# show in browser
+plot.export_html("plots/line_chart.html")
+puts "Chart saved to plots/line_chart.html"
